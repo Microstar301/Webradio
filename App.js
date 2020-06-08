25267585746ca49dff052206710d144e3a2c24b9
Microstar301 <microstar301@gmail.com>
2020-06-08 18:08:17 +0200

    Implemented Testplayer

--- App.js
+++ App.js
@@ -1,27 +1,83 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import RNTrackPlayer from 'react-native-track-player';
 
 export default class Webradio extends Component {
+  state = {
+    textValue: 'Text',
+  };
+  initPlayer = () => {
+    RNTrackPlayer.setupPlayer().then(() => {
+      console.log('Trackplayer set up!');
+      var track = {
+        id: 'unique track id', // Must be a string, required
+
+        url: 'http://rfcmedia2.streamguys1.com/thirdrock.aac', // Load media from the network
+        //url: require('./avaritia.ogg'), // Load media from the app bundle
+        //url: 'file:///storage/sdcard0/Music/avaritia.wav' // Load media from the file system
+
+        title: 'Third Rock Radio',
+        artist: '-',
+        album: 'while(1<2)',
+        genre: '-',
+        date: '2014-05-20T07:00:00+00:00', // RFC 3339
+
+        artwork:
+          'https://thirdrockradio.net/wp-content/uploads/2015/10/TRR_icon.png', // Load artwork from the network
+        //artwork: require('./avaritia.jpg'), // Load artwork from the app bundle
+        //artwork: 'file:///storage/sdcard0/Downloads/artwork.png' // Load artwork from the file system
+      };
+      RNTrackPlayer.add(track).then(function() {
+        console.log('Tracks added');
+      });
+    });
+  };
+
+  stopMusic = () => {
+    RNTrackPlayer.stop();
+  };
+
   initialState = {
-    daText: 'Text',
+    daText: 'Start Music',
+  };
+
+  playMusic = () => {
+    this.initPlayer();
+    RNTrackPlayer.play();
   };
 
   render() {
     return (
       <View style={webradioStyle.mainContainer}>
-        <Text style={webradioStyle.mainText}>{'Yea ' + this.initialState.daText + '!'}</Text>
+        <View style={webradioStyle.mainButton}>
+          <Button
+            title={this.initialState.daText}
+            onPress={this.playMusic}
+            style={webradioStyle.mainButton}
+          />
+        </View>
+        <View style={webradioStyle.mainButton}>
+          <Button
+            title={'STOP MUSIC'}
+            onPress={this.stopMusic}
+            style={webradioStyle.mainButton}
+          />
+        </View>
       </View>
     );
   }
 }
+
 const webradioStyle = StyleSheet.create({
   mainContainer: {
+    marginTop: 16,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFFFF',
   },
-  mainText: {
+  mainButton: {
     fontSize: 50,
+    marginTop: 16,
   },
 });
--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import RNTrackPlayer from 'react-native-track-player';
+
 
 AppRegistry.registerComponent(appName, () => App);
+
+RNTrackPlayer.registerPlaybackService(() => require('./service.js'));

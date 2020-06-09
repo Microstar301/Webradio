c11ab9b5105ad12b0587b60b7cff262bb55db536
Microstar301 <microstar301@gmail.com>
2020-06-09 16:26:22 +0200

    + Current Track displaying * Player state system using native RNTrackerPlayer states

--- service.js
+++ service.js
@@ -1,4 +1,9 @@
+import RNTrackPlayer, {getCurrentTrack} from 'react-native-track-player';
+
 module.exports = async function() {
   // This service needs to be registered for the module to work
   // but it will be used later in the "Receiving Events" section
+  RNTrackPlayer.addEventListener('remote-play', () => RNTrackPlayer.play());
+  RNTrackPlayer.addEventListener('remote-pause', () => RNTrackPlayer.pause());
+  RNTrackPlayer.addEventListener('remote-stop', () => RNTrackPlayer.stop());
 };

1a61e42a8d2e64306eeb0faa2fb049d789e9c3ef
Microstar301 <microstar301@gmail.com>
2020-06-09 11:25:48 +0200

    SQL / Player / Selection working

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, FlatList} from 'react-native';
 import RNTrackPlayer from 'react-native-track-player';
+
+function Item({title, onPress}) {
+  return (
+    <View style={webradioStyle.item}>
+      <Button title={title.toString()} style={'flex:1'} onPress={onPress} />
+    </View>
+  );
+}
 
 export default class Webradio extends Component {
   state = {
@@ -15,28 +23,30 @@ export default class Webradio extends Component {
 
   initPlayer = () => {
     RNTrackPlayer.setupPlayer().then(() => {
-      console.log('Trackplayer set up!');
-      var track = {
-        id: 'unique track id', // Must be a string, required
+      console.debug('Trackplayer set up!');
+    });
+  };
 
-        url: 'http://rfcmedia2.streamguys1.com/thirdrock.aac', // Load media from the network
-        //url: require('./avaritia.ogg'), // Load media from the app bundle
-        //url: 'file:///storage/sdcard0/Music/avaritia.wav' // Load media from the file system
+  addTrack = (title, url, artwork) => {
+    var track = {
+      id: 'unique track id', // Must be a string, required
 
-        title: 'Third Rock Radio',
-        artist: '-',
-        album: 'while(1<2)',
-        genre: '-',
-        date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      url: url, // Load media from the network
+      //url: require('./avaritia.ogg'), // Load media from the app bundle
+      //url: 'file:///storage/sdcard0/Music/avaritia.wav' // Load media from the file system
 
-        artwork:
-          'https://thirdrockradio.net/wp-content/uploads/2015/10/TRR_icon.png', // Load artwork from the network
-        //artwork: require('./avaritia.jpg'), // Load artwork from the app bundle
-        //artwork: 'file:///storage/sdcard0/Downloads/artwork.png' // Load artwork from the file system
-      };
-      RNTrackPlayer.add(track).then(function() {
-        console.log('Tracks added');
-      });
+      title: title,
+      artist: '-',
+      album: '-',
+      genre: '-',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+
+      artwork: artwork, // Load artwork from the network
+      //artwork: require('./avaritia.jpg'), // Load artwork from the app bundle
+      //artwork: 'file:///storage/sdcard0/Downloads/artwork.png' // Load artwork from the file system
+    };
+    RNTrackPlayer.add(track).then(function() {
+      console.debug('Tracks added');
     });
   };
 
@@ -58,6 +68,15 @@ export default class Webradio extends Component {
     }
   };
 
+  playRadio = (title, url, artwork) => {
+    this.initPlayer();
+    this.addTrack(title, url, artwork);
+    RNTrackPlayer.play();
+    this.setState({
+      textValue: 'play',
+    });
+  };
+
   playMusic = () => {
     if (this.state.textValue == 'stop') {
       this.initPlayer();
@@ -73,9 +92,40 @@ export default class Webradio extends Component {
     }
   };
 
+  showJSON = () => {
+    fetch('https://protepto.com/stuff/mot/stations.php')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          dataSource: responseJson,
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   render() {
+    this.showJSON();
     return (
       <View style={webradioStyle.mainContainer}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => (
+            <Item
+              title={item.station_id + ' ' + item.station_name}
+              onPress={() =>
+                this.playRadio(
+                  item.station_name,
+                  item.station_url,
+                  item.station_picture,
+                )
+              }
+            />
+          )}
+          keyExtractor={item => item.station_id}
+        />
+        <Text>{this.state.data}</Text>
         <View style={webradioStyle.mainButton}>
           <Button
             title={this.buttonLabel.startBut}
@@ -121,5 +171,13 @@ const webradioStyle = StyleSheet.create({
   },
   mainText: {
     fontSize: 20,
+  },
+  item: {
+    backgroundColor: '#F0FF00',
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });

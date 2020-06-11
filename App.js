444004c4c5ddf876a9d54dba8c57d441b44f8923
Microstar301 <microstar301@gmail.com>
2020-06-12 00:14:44 +0200

    + added react-native-material-ui + added header and footer + added SVG Icons * changed Design

--- App.js
+++ App.js
@@ -1,32 +1,59 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Button, FlatList, Image,ImageBackground,TouchableOpacity} from 'react-native';
-import RNTrackPlayer from 'react-native-track-player';
 
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  StatusBar,
+  ScrollView,
+} from 'react-native';
+import RNTrackPlayer from 'react-native-track-player';
+import {Button, ThemeProvider} from 'react-native-elements';
+import Icon from 'react-native-material-ui/src/Icon';
+
+// ON ERROR INSTALL THIS:
+// https://github.com/xotahal/react-native-material-ui/blob/master/docs/GettingStarted.md
+// npm install --save react-native-material-ui
+
+//<Image source={{uri: {thumb}.thumb}} style={styles.pic} />
 function Item({title, onPress, thumb}) {
   return (
-    <View style={webradioStyle.item}>
-      <Button title={title.toString()} style={'flex:1'} onPress={onPress} />
-      <Image source={{uri: {thumb}.thumb}} style={webradioStyle.pic} />
+    <View style={styles.items}>
+      <Button
+        title={title.toString()}
+        style={styles.buttonStation}
+        onPress={onPress}
+        rounded={true}
+        icon={<Image source={{uri: {thumb}.thumb}} style={styles.pic} />}
+      />
     </View>
   );
 }
 
-const background_image = { uri: "https://thumbs.dreamstime.com/z/abstract-geometric-background-modern-overlapping-triangles-unusual-color-shapes-your-message-business-tech-presentation-app-53265512.jpg" };
-const radio_image_play ={uri:"https://protepto.com/stuff/mot/play.png"}
-const radio_image_stop ={uri:"https://protepto.com/stuff/mot/stop.png"}
-const radio_image_pause ={uri:"https://protepto.com/stuff/mot/pause.png"}
-
-
+const theme = {
+  colors: {
+    primary: '#570091',
+  },
+};
 
 export default class Webradio extends Component {
+  Webradio() {
+    this.updateJSON();
+  }
+
   state = {
-    textValue: '',
     curTrack: '',
+    curImg: 'https://protepto.com/stuff/mot/note.png',
   };
 
   buttonLabel = {
-    pauseTogg: 'Pause Music',
-    stopBut: 'Stop Music',
+    pauseIcon: 'play-arrow',
+    pauseTogg: 'Play',
+    stopBut: 'stop',
   };
 
   initPlayer = () => {
@@ -49,50 +76,60 @@ export default class Webradio extends Component {
   };
 
   stopMusic = async () => {
-    var state = await RNTrackPlayer.getState();
+    const state = await RNTrackPlayer.getState();
     if (
       state == RNTrackPlayer.STATE_PLAYING ||
       state == RNTrackPlayer.STATE_PAUSED
     ) {
-      RNTrackPlayer.stop();
+      await RNTrackPlayer.stop();
+      this.setPlay();
       this.setState({
+        curImg: 'https://protepto.com/stuff/mot/note.png',
         curTrack: '',
       });
-      this.updateTrack();
+      await this.updateTrack();
+    } else {
+      this.onLaunch();
     }
+  };
+
+  setPause = () => {
+    this.buttonLabel.pauseTogg = 'Pause';
+    this.buttonLabel.pauseIcon = 'pause';
+  };
+
+  setPlay = () => {
+    this.buttonLabel.pauseTogg = 'Play';
+    this.buttonLabel.pauseIcon = 'play-arrow';
   };
 
   togglePauseMusic = async () => {
-    var state = await RNTrackPlayer.getState();
+    const state = await RNTrackPlayer.getState();
     if (state == RNTrackPlayer.STATE_PLAYING) {
-      RNTrackPlayer.pause();
-      this.setState({
-        textValue: 'pause',
-      });
-      this.buttonLabel.pauseTogg = 'Play Music';
+      await RNTrackPlayer.pause().then(this.onLaunch());
     } else if (state == RNTrackPlayer.STATE_PAUSED) {
-      RNTrackPlayer.play();
-      this.buttonLabel.pauseTogg = 'Pause Music';
+      await RNTrackPlayer.play().then(this.onLaunch);
     }
   };
-
   playRadio = async (tid, title, url, artwork) => {
     this.initPlayer();
     this.addTrack(tid, title, url, artwork);
-    RNTrackPlayer.play();
-    this.updateTrack();
+    await RNTrackPlayer.play();
+    this.setPause();
+    await this.updateTrack();
     //this.setState({
     //  curTrack: await RNTrackPlayer.getCurrentTrack(),
     //});
   };
 
-  showJSON = () => {
+  updateJSON = () => {
     fetch('https://protepto.com/stuff/mot/stations.php')
       .then(response => response.json())
       .then(responseJson => {
         this.setState({
           dataSource: responseJson,
         });
+        this.updateTrack();
       })
       .catch(error => {
         console.error(error);
@@ -100,124 +137,217 @@ export default class Webradio extends Component {
   };
 
   updateTrack = async () => {
-    var curTID = await RNTrackPlayer.getCurrentTrack();
-    var curSname;
-    this.state.dataSource.map(function(station) {
-      if (station.station_id == curTID) {
-        curSname = station.station_name;
+    const curTID = await RNTrackPlayer.getCurrentTrack();
+    let curStation = {};
+    if (this.state.dataSource !== undefined) {
+      this.state.dataSource.map(function(station) {
+        if (station.station_id == curTID) {
+          curStation = {
+            name: station.station_name,
+            img: station.station_picture,
+          };
+        }
+      });
+      if (curStation.name != undefined) {
+        this.setState({
+          curTrack: curStation.name,
+          curImg: curStation.img,
+        });
+      } else {
+        this.setState({
+          curTrack: '',
+          curImg: 'https://protepto.com/stuff/mot/note.png',
+        });
       }
-    });
-    this.setState({
-      curTrack: curSname,
-    });
+    } else {
+      this.setState({
+        curTrack: '',
+        curImg: 'https://protepto.com/stuff/mot/note.png',
+      });
+    }
   };
 
+  onLaunch = async () => {
+    const state = await RNTrackPlayer.getState();
+    if (
+      state == RNTrackPlayer.STATE_PAUSED ||
+      state == RNTrackPlayer.STATE_STOPPED
+    ) {
+      this.setPlay();
+      if (state == RNTrackPlayer.STATE_STOPPED) {
+        this.setState({
+          curImg: 'https://protepto.com/stuff/mot/note.png',
+          curTrack: '',
+        });
+      }
+    } else if (state == RNTrackPlayer.STATE_PLAYING) {
+      this.setPause();
+    }
+    await this.updateTrack();
+  };
+
+  componentDidMount() {
+    this.updateJSON();
+    this.onLaunch().then(() => null);
+  }
+
+  // RENDERER
   render() {
-    this.showJSON();
     return (
-      <View style={webradioStyle.mainContainer}>
-        <ImageBackground source={background_image} style={webradioStyle.background_image}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => (
-            <Item
-              title={item.station_id + ' ' + item.station_name}
-              onPress={() =>
-                this.playRadio(
-                  item.station_id,
-                  item.station_name,
-                  item.station_url,
-                  item.station_picture,
-                )
-              }
-              thumb={item.station_picture}
-            />
-          )}
-          keyExtractor={item => item.station_id}
-        />
-        <View style={webradioStyle.mainButton}>
-
-          <TouchableOpacity
-              style={webradioStyle.button_controls}
-              onPress={this.togglePauseMusic}>
-
-            <Image  source={radio_image_play} style={webradioStyle.radioPic} />
-          </TouchableOpacity>
-
-          <TouchableOpacity
-              style={webradioStyle.button_controls}
-              onPress={this.stopMusic}>
-
-            <Image  source={radio_image_stop} style={webradioStyle.radioPic} />
-          </TouchableOpacity>
-
+      <ThemeProvider theme={theme}>
+        <View style={styles.container}>
+          <StatusBar
+            backgroundColor="#9900ff"
+            barStyle={'light-content'}
+            hidden={false}
+          />
+          <View style={[styles.header]}>
+            <Text style={styles.title}>
+              <Icon name={'radio'} color={'white'} />
+              Webradio
+            </Text>
+          </View>
+          <FlatList
+            data={this.state.dataSource}
+            renderItem={({item}) => (
+              <Item
+                title={item.station_name}
+                onPress={() =>
+                  this.playRadio(
+                    item.station_id,
+                    item.station_name,
+                    item.station_url,
+                    item.station_picture,
+                  )
+                }
+                thumb={item.station_picture}
+              />
+            )}
+            keyExtractor={item => item.station_id}
+            style={styles.list}
+          />
+          <View style={[styles.footer]}>
+            <View style={styles.controlButtons}>
+              <View style={{flexDirection: 'row'}}>
+                <Image
+                  source={{uri: this.state.curImg}}
+                  style={{
+                    width: 64,
+                    height: 64,
+                    marginLeft: 16,
+                    resizeMode: 'contain',
+                    marginRight: 8,
+                  }}
+                />
+                <Text style={styles.trackName}>{this.state.curTrack}</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <View style={{marginRight: 8, elevation: 2}}>
+                  <Button
+                    icon={{
+                      name: this.buttonLabel.stopBut,
+                      size: 32,
+                      color: 'black',
+                    }}
+                    type={'clear'}
+                    onPress={this.stopMusic}
+                    buttonStyle={[styles.button_controls]}
+                  />
+                </View>
+                <Button
+                  icon={{
+                    name: this.buttonLabel.pauseIcon,
+                    size: 32,
+                    color: 'black',
+                  }}
+                  type={'clear'}
+                  onPress={this.togglePauseMusic}
+                  buttonStyle={[styles.button_controls]}
+                />
+              </View>
+            </View>
+            <View style={styles.controlButtons} />
+          </View>
         </View>
-        <View style={webradioStyle.mainButton}>
-
-          <Text style={webradioStyle.mainText}>{this.state.curTrack}</Text>
-        </View>
-        </ImageBackground>
-      </View>
+      </ThemeProvider>
     );
   }
 }
 
-const webradioStyle = StyleSheet.create({
-  mainContainer: {
-    marginTop: 16,
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  mainButton: {
+  header: {
+    height: 100,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    backgroundColor: '#9900ff',
+    zIndex: 10,
+    elevation: 10,
+  },
+  footer: {
+    height: 100,
+    position: 'absolute',
+    backgroundColor: '#ffffff',
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    elevation: 2,
+    zIndex: 2,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  controlButtons: {
     marginTop: 16,
-    width: '100%',
+    marginRight: 16,
     flexDirection: 'row',
     display: 'flex',
-    justifyContent: 'flex-end',
-
-
+    justifyContent: 'space-between',
   },
-  mainText: {
+  trackName: {
+    paddingTop: 16,
+    paddingRight: 16,
     fontSize: 20,
     color: '#000000',
     marginBottom: 8,
-  },
-  item: {
-    backgroundColor: '#F0FF00',
-    marginVertical: 8,
-    marginHorizontal: 16,
-
+    justifyContent: 'space-between',
   },
   title: {
+    color: '#ffffff',
+    backgroundColor: '#9900ff',
+    padding: 8,
     fontSize: 32,
+    margin: 16,
+    fontFamily: 'Roboto',
+    letterSpacing: 16,
+    textAlign: 'center',
+  },
+  list: {
+    marginTop: 100,
+    marginBottom: 100,
+  },
+  button_controls: {
+    elevation: 0,
+    width: 64,
+    height: 64,
   },
   pic: {
-    flex:1,
     height: 64,
-    resizeMode: 'cover',
-    top: 0, left: 0,
-    right: 0, bottom: 0,
-
+    width: 64,
+    resizeMode: 'contain',
+    marginRight: 16,
   },
-  background_image: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
+  buttonStation: {
+    justifyContent: 'space-between',
   },
-  button_controls:{
-    borderWidth:1,
-    borderColor:'rgba(0,0,0,0.2)',
-    alignItems:'center',
-    justifyContent:'center',
-    width:86,
-    height:86,
-    backgroundColor:'#fff',
-    borderRadius:43,
+  items: {
+    borderRadius: 100,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  radioPic:{
-    flex: 1,
-    width: '100%',
-    height: '100%',
-  }
 });

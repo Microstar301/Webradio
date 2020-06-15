704a9ed08d520b03a43926edcf3eb894b2d68b45
Microstar301 <microstar301@gmail.com>
2020-06-15 15:49:28 +0200

    Current Version without navigation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './App.js';
 import {name as appName} from './app.json';
 import RNTrackPlayer from 'react-native-track-player';
 

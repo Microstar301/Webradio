5ab74ddd8f36adeb2da9ce8f43977fc075f302aa
Microstar301 <microstar301@gmail.com>
2020-06-16 11:33:52 +0200

    + added Navigation via react-navigation * changed Codebase * App.js + allstations.js + favorites.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App.js';
 import {name as appName} from './app.json';
 import RNTrackPlayer from 'react-native-track-player';
+import App from './App';
 
 AppRegistry.registerComponent(appName, () => App);
 
